127f604c135863dd94a3e6e8882d1913906c582c
Kai Tusa <kai.tusa@unikie.com>
2025-08-22 08:47:56 +0300

    Remove generic x86 target
    Signed-off-by: Kai Tusa <kai.tusa@unikie.com>

--- scripts/check-all-targets.sh
+++ scripts/check-all-targets.sh
@@ -36,7 +36,6 @@ BASE_URL="$1"
 subdirectories=(
     "aarch64-linux.nvidia-jetson-orin-agx-debug/"
     "aarch64-linux.nvidia-jetson-orin-nx-debug/"
-    "x86_64-linux.generic-x86_64-debug/"
     "x86_64-linux.lenovo-x1-carbon-gen11-debug/"
     "x86_64-linux.nvidia-jetson-orin-agx-debug-from-x86_64/"
     "x86_64-linux.nvidia-jetson-orin-nx-debug-from-x86_64/"
