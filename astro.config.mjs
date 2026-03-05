9c7cb374653c26a36b5ddf9ddb83a3a1b10a5c68
Joonas Rautiola <joonas.rautiola@unikie.com>
2026-03-05 17:03:52 +0200

    Fix stretched site favicon
    Signed-off-by: Joonas Rautiola <joonas.rautiola@unikie.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,15 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import favicons from "astro-favicons";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   output: "static",
   site: "https://archive.vedenemo.dev",
+  integrations: [favicons()],
 });
